0ef667f345885a824a8eeb6bc91c78f00cb16f53
gmsantos <gabriel.ms1@hotmail.com>
2020-01-06 22:12:33 -0300

    configure app setting on controller

--- src/main/java/hello/HelloController.java
+++ src/main/java/hello/HelloController.java
@@ -1,14 +1,18 @@
 package hello;
 
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @RestController
 public class HelloController {
-	
+
+	@Value("${app.title}")
+	private String appTitle;
+
 	@RequestMapping("/")
 	public String index() {
-		return "Greetings from Spring Boot!";
+		return "Greetings from Spring Boot! " + appTitle;
 	}
 	
 }
